31bbeb48a77e395d0834938a5e884898fae41a16
Andreas Moreno Jønsson <andreas.ecuador@live.dk>
2024-08-18 15:04:46 +0200

    redeploy uden birdeye

--- src/buy/price.rs
+++ src/buy/price.rs
@@ -160,22 +160,11 @@ pub async fn calculate_pump_curve_price(state: &BondingCurveLayout) -> f64 {
 }
 
 pub async fn get_current_sol_price() -> Result<f64, Box<dyn Error>> {
-    let url =
-        "https://public-api.birdeye.so/defi/price?address=So11111111111111111111111111111111111111112";
-    let birdeye_api_key = std::env
-        ::var("BIRDEYE_API")
-        .expect("You must set the RPC_URL environment variable!");
-    let response = reqwest::Client
-        ::new()
-        .get(url)
-        .header("X-API-KEY", birdeye_api_key)
-        .send().await?;
+    let url = "https://api.coingecko.com/api/v3/simple/price?ids=solana&vs_currencies=usd";
+    let response = reqwest::get(url).await?;
 
-    if response.status().is_success() {
-        let sol_price_json: serde_json::Value = response.json().await?;
-        let sol_price_usd: f64 = sol_price_json["data"]["value"].as_f64().unwrap_or(0.0);
-        Ok(sol_price_usd)
-    } else {
-        Err("Failed to fetch SOL price".into())
-    }
+    let json: serde_json::Value = response.json().await?;
+    let price = json["solana"]["usd"].as_f64().unwrap_or(0.0);
+
+    Ok(price)
 }
--- src/sell/price.rs
+++ src/sell/price.rs
@@ -1,26 +1,11 @@
 use std::error::Error;
 
 pub async fn get_current_sol_price() -> Result<f64, Box<dyn Error>> {
-    let url =
-        "https://public-api.birdeye.so/defi/price?address=So11111111111111111111111111111111111111112";
+    let url = "https://api.coingecko.com/api/v3/simple/price?ids=solana&vs_currencies=usd";
+    let response = reqwest::get(url).await?;
 
-    let birdeye_api_key = std::env
-        ::var("BIRDEYE_API")
-        .expect("You must set the RPC_URL environment variable!");
-    // Make GET request to the API endpoint with API key in header
-    let response = reqwest::Client
-        ::new()
-        .get(url)
-        .header("X-API-KEY", birdeye_api_key)
-        .send().await?;
+    let json: serde_json::Value = response.json().await?;
+    let price = json["solana"]["usd"].as_f64().unwrap_or(0.0);
 
-    // Check if response is successful
-    if response.status().is_success() {
-        // Parse the JSON response to extract the SOL price
-        let sol_price_json: serde_json::Value = response.json().await?;
-        let sol_price_usd: f64 = sol_price_json["data"]["value"].as_f64().unwrap_or(0.0);
-        Ok(sol_price_usd)
-    } else {
-        Err("Failed to fetch SOL price".into()) // Convert string to boxed Error trait object
-    }
+    Ok(price)
 }
